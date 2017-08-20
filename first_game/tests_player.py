12c2552aa20425426466406cbe89ac0c6288bfa4
David Shaffer <davepshaffer@yahoo.com>
2017-08-20 18:57:32 -0500

    added unit tests for Player

--- first_game/tests_player.py
+++ first_game/tests_player.py
@@ -10,7 +10,12 @@ class PlayerTests(unittest.TestCase):
     def the_space_key_begins_a_jump(self):
         vertical_velocity = Player._maintain_jump(key_pressed={K_RIGHT: False, K_LEFT: False, K_SPACE: True},
                                                   on_top=True, vertical_velocity=0, default_vertical_velocity=1000)
-        self.assertEqual(vertical_velocity, -460)  # -500 discounted 40
+        self.assertEqual(vertical_velocity, -460)  # -500 + 40
+
+    def can_not_jump_in_mid_air(self):
+        vertical_velocity = Player._maintain_jump(key_pressed={K_RIGHT: False, K_LEFT: False, K_SPACE: True},
+                                                  on_top=False, vertical_velocity=0, default_vertical_velocity=1000)
+        self.assertEqual(vertical_velocity, 40)  # 0 + 40
 
 
 if __name__ == '__main__':
