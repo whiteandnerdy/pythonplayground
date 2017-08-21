3241237ec2c30c548dbd7ae93777b5df6d520294
David Shaffer <davepshaffer@yahoo.com>
2017-08-20 20:55:32 -0500

    added unit tests for Player

--- first_game/tests_player.py
+++ first_game/tests_player.py
@@ -1,3 +1,4 @@
+from collections import namedtuple
 from pygame import image, rect, key, K_LEFT, K_RIGHT, K_SPACE
 from player import Player
 import unittest
@@ -16,6 +17,25 @@ class PlayerTests(unittest.TestCase):
         vertical_velocity = Player._maintain_jump(key_pressed={K_RIGHT: False, K_LEFT: False, K_SPACE: True},
                                                   on_top=False, vertical_velocity=0, default_vertical_velocity=1000)
         self.assertEqual(vertical_velocity, 40)  # 0 + 40
+
+    def it(self):
+        class Layer:
+            def collide(self, a, b):
+                return [Boundary()]
+
+        class Boundary:
+            left = 0
+            right = 0
+            bottom = 0
+            top = 0
+
+            def __getitem__(self, item):
+                return 'tlrb'
+
+        Layer().collide(None, None)
+
+        Rect = namedtuple('Rect', 'right left bottom top')
+        new_rect, on_top = Player._get_new_position_considering_boundaries(Rect(0, 0, 0, 0), Rect(0, 0, 0, 0), Layer())
 
 
 if __name__ == '__main__':
