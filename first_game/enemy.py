7f2209a25b8a9b11bae1ee78d3841e5aa8170119
David Shaffer <davepshaffer@yahoo.com>
2017-09-02 23:53:29 -0500

    created basic bouncing enemy

--- first_game/enemy.py
+++ first_game/enemy.py
@@ -1,33 +1,47 @@
 import pygame
+from outboundary import OutBoundary
 
 
 class Enemy(pygame.sprite.Sprite):
     image = pygame.image.load('resources/enemy.png')
 
     def __init__(self, location, *groups):
-        super(Enemy, self).__init__(*groups)
+        super().__init__(*groups)
+
         self.rect = pygame.rect.Rect(location, self.image.get_size())
-        self.last_direction = 'down'
+        self.vertical_velocity = self.default_vertical_velocity = 400
 
     def update(self, dt, tilemap, keys_pressed, player):
-        if self.last_direction == 'down':
-            self.rect.y += 100 * dt
-        else:
-            self.rect.y -= 100 * dt
+        new_rect = self._get_new_position_without_boundaries(dt, self.rect, self.vertical_velocity)
 
-        colliding_cells = tilemap.layers['triggers'].collide(self.rect, 'blockers') + \
-                          tilemap.layers['triggers'].collide(self.rect, 'reverse')
+        boundaries = [OutBoundary(boundary_object) for boundary_object in
+                      tilemap.layers['triggers'].collide(new_rect, 'blockers')
+                      if boundary_object['blockers'] == 'out']
 
-        if len(colliding_cells) > 0:
-            cell = colliding_cells[0]
+        on_top = False
+        for boundary in boundaries:
+            new_rect, on_top = boundary.stick_and_get_new_position(self.rect, new_rect, on_top)
 
-            # don't overshoot the boundary
-            if self.last_direction == 'down':
-                self.rect.bottom = cell.top
-            else:
-                self.rect.top = cell.bottom
+        self.vertical_velocity = self._maintain_jump(on_top, self.vertical_velocity, self.default_vertical_velocity)
 
-            self.last_direction = 'up' if self.last_direction == 'down' else 'down'
+        self.rect = new_rect
 
         if self.rect.colliderect(player.rect):
             player.is_dead = True
+
+    @staticmethod
+    def _get_new_position_without_boundaries(dt, new_rect, vertical_velocity):
+        new_rect = new_rect.copy()
+
+        new_rect.y += vertical_velocity * dt
+
+        return new_rect
+
+    @staticmethod
+    def _maintain_jump(on_top, vertical_velocity, default_vertical_velocity):
+        if on_top:  # don't jump from mid-air, you must be standing on top of something
+            vertical_velocity = -500
+
+        # turn jump into gravity over time by degrading the vertical velocity
+        vertical_velocity = min(vertical_velocity + 40, default_vertical_velocity)
+        return vertical_velocity
